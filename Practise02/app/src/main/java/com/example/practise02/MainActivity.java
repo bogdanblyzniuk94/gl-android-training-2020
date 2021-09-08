4ef87cd804b45f6cb4b8bfa7cf0333e154021173
Bohdan Blyzniuk <bogdanblyzniuk@i.ua>
2021-09-08 17:19:18 +0300

    Add Android App of Practise02

---
+++ Practise02/app/src/main/java/com/example/practise02/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.practise02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(final View view) {
+        Toast.makeText(this, "Button clicked!", Toast.LENGTH_LONG).show();
+    }
+}
